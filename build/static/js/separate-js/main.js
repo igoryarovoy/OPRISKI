7393e533d6ec5827dc094a11ff545b1610d096d1
Igor Yarovoy <yarovoy.ti@gmail.com>
2017-01-26 01:42:38 +0200

    added elements, build new page

--- build/static/js/separate-js/main.js
+++ build/static/js/separate-js/main.js
@@ -8,30 +8,28 @@ $(document).ready(function() {
 		dots: true,
 		// autoplay: true,
 		// autoplaySpeed: 5000,
-		// lazyLoad: 'ondemand'
+		lazyLoad: 'ondemand'
 	});
-    
-	// $('.js-view-slider').slick({
-	// 	slidesToShow: 1,
-	// 	slidesToScroll: 1,
-	// 	arrows: false,
-	// 	draggable: false,
-	// 	fade: true,
-	// 	autoplay: true,
-	// 	autoplaySpeed: 5000,
-	// 	lazyLoad: 'ondemand',
-	// 	asNavFor: '.js-nav-slider'
-	// });
 
-	// $('.js-nav-slider').slick({
-	// 	slidesToShow: 5,
-	// 	slidesToScroll: 1,
-	// 	arrows: false,
-	// 	focusOnSelect: true,
-	// 	draggable: false,
-	// 	vertical: true,
-	// 	asNavFor: '.js-view-slider'
-	// });
+
+	$('.js-view').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		draggable: true,
+		dots: true,
+		fade: true,
+		lazyLoad: 'ondemand',
+		asNavFor: '.js-nav-slider'
+	});
+
+	$('.js-nav-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		focusOnSelect: true,
+		lazyLoad: 'ondemand',
+		asNavFor: '.js-view'
+	});
 
 
 	$('.js-modal').on('show.bs.modal', function () {
